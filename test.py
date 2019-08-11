da34574a4ab78067f4c28616058344f0bb91f488
Alexander Temme <alexander.temme@gmx.de>
2019-08-11 21:03:27 +0200

    updated test file

--- test.py
+++ test.py
@@ -1,19 +1,28 @@
-from propygate.model import FeedForward
-from propygate.layers import FullyConnected
+from propygate import network, layers, optimizers, losses, datasets, utils
 import numpy as np
 
 
 def main():
 
-    model = FeedForward()
-    model.add(FullyConnected(20, activation="relu", input_dim=5))
-    model.add(FullyConnected(30, activation="relu"))
-    model.add(FullyConnected(10, activation="sigmoid"))
+    model = network.FeedForward()
+    model.add(layers.FullyConnected(20, activation="relu", input_dim=(28, 28)))
+    model.add(layers.FullyConnected(30, activation="relu"))
+    model.add(layers.FullyConnected(10, activation="sigmoid"))
 
-    model.initialize()
+    optimizer = optimizers.GradientDescent(1e-3)
+    loss = losses.MSE()
+    model.initialize(optimizer, loss)
 
-    print(model.layers[0]._fprop(np.ones(5)).shape)
     print(model)
+
+    (x_train, x_test, x_valid), (y_train, y_test, y_valid) = datasets.load_mnist()
+
+    model.train(x_train[:1000], y_train[:1000], epochs=10, batch_size=64,
+                val_data=(x_valid, y_valid))
+    model.plot_metrics()
+
+    prediction = model.predict(x_test[0])
+    utils.plot_example(x_test[0], prediction)
 
 
 if __name__ == "__main__":
